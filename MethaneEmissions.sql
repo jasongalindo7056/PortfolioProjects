52abdb5dc0c991fc01fdbca89eba59ba05280be9
jasongalindo7056 <96474350+jasongalindo7056@users.noreply.github.com>
2022-08-20 11:28:17 -0700

    Add files via upload

---
+++ MethaneEmissions.sql
@@ -0,0 +1,29 @@
+#Highest emissions
+SELECT *
+FROM emissions
+WHERE emissions = (SELECT MAX(emissions) FROM emissions);
+
+#Total methane emissions per Country
+SELECT country, SUM(emissions)
+FROM emissions
+GROUP BY country
+ORDER BY AVG(emissions) DESC;
+
+#Total methane emissions per segment
+SELECT segment, SUM(emissions)
+FROM emissions
+GROUP BY segment
+ORDER BY SUM(emissions) DESC;
+
+#Total methane emissions per Region
+SELECT region, SUM(emissions)
+FROM emissions
+GROUP BY region
+ORDER BY SUM(emissions) DESC;
+
+#Average methane emissions per Country
+SELECT country, AVG(emissions)
+FROM emissions
+GROUP BY country
+ORDER BY AVG(emissions) DESC;
+
